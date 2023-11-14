339775277de12118d062885d6e88b5fc29163f7f
kingcoder91 <kingkwofie@gmail.com>
2023-11-14 14:08:37 -0800

    printf group project

--- handle_prints.c
+++ handle_prints.c
@@ -1,21 +1,22 @@
 #include "main.h"
 /**
- * handle_print - this function prints an argument based on its type.
- * @fmt: formatted string in which to print the arguments
- * @list: list of arguments to be printed.
+ * handle_print - this function prints an argument based on its type
+ * @fmt: Formatted string in which to print the arguments.
+ * @list: List of arguments to be printed.
  * @ind: ind.
- * @buffer: buffer array to handle print.
- * @flags: calculates active flags
+ * @buffer: Buffer array to handle print.
+ * @flags: Calculates active flags
  * @width: get width.
- * @precision: precision specification
- * @size: size specifier
- * Return:(1 or 2)
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: (1 or 2)
  */
 
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
+
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
