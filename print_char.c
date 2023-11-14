bb4280512dc910b2506a48a99fed7f879e686316
Timothy Ofie <timzblue@gmail.com>
2023-11-13 16:03:17 -0800

    task 0 to task 6

---
+++ print_char.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_char - prints a character
+ * @val: arguments
+ * Return: an integer.
+ */
+int print_char(va_list val)
+{
+	char s;
+
+	s = va_arg(val, int);
+	_putchar(s);
+	return (1);
+}
